485782e9de61f0c06ad4e7aa1d9891c0b71cc42a
savagetourist <44285926+savagetourist@users.noreply.github.com>
2022-04-19 19:21:37 +0700

    Update ToggleAsset.php
    updated bootstrap

--- ToggleAsset.php
+++ ToggleAsset.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace lo\widgets;
+namespace st\widgets;
 
 use yii\web\AssetBundle;
 
@@ -18,6 +18,6 @@ class ToggleAsset extends AssetBundle
 	
     public $depends = [
         'yii\web\JqueryAsset',
-        'yii\bootstrap\BootstrapAsset'
+        'yii\bootstrap4\BootstrapAsset'
     ];
 }
